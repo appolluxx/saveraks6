a565f1a040a8afc81215ad2e490ce4d9235d3960
POLLUX <hallo.hi.prem@gmail.com>
2026-01-07 21:40:01 +0700

    build: increase chunk size limit and optimize chunks

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,5 +9,17 @@ export default defineConfig({
         fs: {
             allow: ['..']
         }
+    },
+    build: {
+        chunkSizeWarningLimit: 2000,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                }
+            }
+        }
     }
 });
